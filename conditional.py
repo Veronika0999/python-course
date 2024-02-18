0ea067979b8850a8a3fe51d4d7ea339081ea4d59
Veronika Krivankova <vercakrivankova@seznam.cz>
2024-02-18 16:01:26 +0100

    practice conditional

---
+++ conditional.py
@@ -0,0 +1,9 @@
+# Bouncer Code-Along and Nested Conditionals
+age = int(input('How old are you: '))
+
+if age >= 21:
+    print("You are good to enter and can drink")
+elif age >= 18:
+    print("You can enter but need a wristband")
+else:
+    print("You can't came in, little one")
