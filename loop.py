46894b33bc75ddddd88bca7a3d51a23cae231857
Veronika Krivankova <vercakrivankova@seznam.cz>
2024-02-20 18:23:47 +0100

    practice loop

---
+++ loop.py
@@ -0,0 +1,13 @@
+times = int(input("How many times do I have to tell you? "))
+
+for y in range(times):
+   print("CLEAN UP YOUR ROOM")
+
+
+for number in range(1, 21):
+    if number == 4 or number == 13:
+        print(f"{number} is UNLUCKY!")
+    elif number % 2 == 0:
+        print(f"{number} is even")
+    else:
+        print(f"{number} is odd")
